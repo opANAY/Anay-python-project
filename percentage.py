42fb73707d924a509bd1c1e1b80e6eb3f93d854a
opANAY <anaygupta2021@gmail.com>
2026-05-27 20:27:14 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,10 @@
+print("THE FOLLOWING MARKS OUT OF HUNDRED")
+science=80
+math=70
+coding=73
+english=83
+social=82
+total_marks=science+math+coding+english+social
+print("Total marks obtained: ",total_marks)
+percentage=(total_marks/500)*100
+print("percentage",percentage)
