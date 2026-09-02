989abe9f893b5acfca06e9ba841cf994abeb1dea
opANAY <anaygupta2021@gmail.com>
2026-09-02 20:15:21 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,22 @@
+secret_number=28
+guess=int(input)
+int("input(guess a number")
+if_not_guessed_in_5_attempts
+reveal(secret_number)
+if_guess >=1 or guess <10
+print("ice_cold")
+if_guess >=11 or guess <20
+print("cold")
+if_guess >=20 or guess <30
+print("hot")
+if_guess >=30 or guess <35
+print("warm")
+if_guess >=35 or guess <45
+print("cold")
+if_guess >=45 or guess <50
+print("ice_cold")
+every_wrong_attempt_remove_1heart_out_of_5hearts
+
+
+ 
+
